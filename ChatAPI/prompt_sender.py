adc43c792d4a27bb477292a85d6493c52d1d4469
jyzl2020 <2326520660@qq.com>
2023-08-16 16:50:28 +0800

    11

--- ChatAPI/prompt_sender.py
+++ ChatAPI/prompt_sender.py
@@ -34,6 +34,15 @@ class Sender:
     def send(self, prompt):
         header = {
             'authorization': self.authorization,
+            'sec-fetch-dest': 'empty',
+            'origin': 'https://discord.com',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'x-debug-options': 'bugReporterEnabled',
+            'x-discord-locale': 'zh-CN',
+            'x-discord-timezone': 'Asia/Shanghai',
+            'referer': 'https://discord.com/channels/943543313368223754/943543313368223760',
             # 'Connection': 'close'
         }
         payload = {'type': 2,
